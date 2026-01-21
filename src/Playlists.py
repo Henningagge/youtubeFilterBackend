20ad78a16934b6b1813a85c9ea9437a2c1f5f0f3
heagge <henning.agge@gmail.com>
2026-01-21 19:56:46 +0100

    added error handeling for getPlaylistsViaChannelId

--- src/Playlists.py
+++ src/Playlists.py
@@ -12,12 +12,17 @@ def getPlaylistViaChannelId():
     """
     api_service_name = "youtube"
     api_version = "v3"
-    youtube = googleapiclient.discovery.build(api_service_name,api_version, developerKey=Api_Key)
-    request = youtube.playlists().list(
-        part="contentDetails",
-        channelId=currentTopicChannelId,
-    )
-    response = request.execute()
+    try:
+        youtube = googleapiclient.discovery.build(api_service_name,api_version, developerKey=Api_Key)
+        request = youtube.playlists().list(
+            part="contentDetails",
+            channelId=currentTopicChannelId,
+        )
+        response = request.execute()
+    except Exception as e:
+        print(f"There has been an error when requesting the Youtube Api for the Playlists of a Channel error: {e}")
+        return []
+
     playlistIds = []
     for item in response["items"]:
         playlistIds.append(item["id"])
