3bd8e9ec2804df29545e221b2e9fd6691a1c94f4
heagge <henning.agge@gmail.com>
2026-02-04 19:29:40 +0100

    database now with all starter ids

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from recomendations import getChannelRecource, getSubscribedChannels
+from recomendations import getChannelRecource, getSubscribedChannels, getLightChannelResource
 from variable import currentTopicChannelId
 
 
@@ -6,6 +6,10 @@ def main():
     recomendations = getSubscribedChannels(currentTopicChannelId)
     print(recomendations)
     # result = getPlaylistViaChannelId()
+    channel = getChannelRecource("UCFbNIlppjAuEX4znoulh0Cw")
+    print(channel)
+    light = getLightChannelResource("UCFbNIlppjAuEX4znoulh0Cw")
+    print(light)
     pass
 
     # result2 = openPlaylist("PLg7eNtqimWhwQsTr__Npprv7O26GBC8Yf")
