304b691a064c4986ff0172860393522695066eb2
heagge <henning.agge@gmail.com>
2026-02-04 19:52:16 +0100

    add more context

--- src/Ai/searchImprover.py
+++ src/Ai/searchImprover.py
@@ -2,11 +2,10 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import time
-
+load_dotenv()
+ai_api_key = os.environ.get("GEMINI_API_KEY")
 
 def fixSpelling(query):
-    load_dotenv()
-    ai_api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=ai_api_key)
     response = client.models.genrate_content(
@@ -22,3 +21,24 @@ def fixSpelling(query):
     )
     return response.text
 
+def improveQuery(query):
+
+    client = genai.Client(api_key=ai_api_key)
+    response = client.models.genrate_content(
+        model="gemini-2.5-flash-lite",
+        content=f"""
+        Rewrite this Youtube Search query to be more specific and searchbal.
+
+        Original: {query}
+
+        Consider:
+        - That the person that write the Query is interested in Tech and Programming.
+        - Keep it concise (under 10 words)
+        - It should be a google style search query that's very specific
+        - Don't use boolean logic
+
+
+        Return the query in this format:
+        Rewritten query: Query"""
+    )
+    return response.text
