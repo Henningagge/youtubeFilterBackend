5dbe4dfe13df3351f5cb20cbd2a00647d7245201
heagge <henning.agge@gmail.com>
2026-02-06 08:35:11 +0100

    fiixed issues for flake8

--- src/main.py
+++ src/main.py
@@ -3,17 +3,17 @@ from variable import currentTopicChannelId
 from AIDBHandler import addNewChannelToDB
 
 def main():
-    #recomendations = getSubscribedChannels(currentTopicChannelId)
-    #print(recomendations)
-    # result = getPlaylistViaChannelId()
-   # channel = getChannelRecource("UCFbNIlppjAuEX4znoulh0Cw")
-    #print(channel)
-    #light = getLightChannelResource("UCFbNIlppjAuEX4znoulh0Cw")
-   # print(light)
+    recomendations = getSubscribedChannels(currentTopicChannelId)
+    print(recomendations)
+    result = getPlaylistViaChannelId()
+    channel = getChannelRecource("UCFbNIlppjAuEX4znoulh0Cw")
+    print(channel)
+    light = getLightChannelResource("UCFbNIlppjAuEX4znoulh0Cw")
+    print(light)
     addNewChannelToDB("UCFbNIlppjAuEX4znoulh0Cw")
     pass
-
-    # result2 = openPlaylist("PLg7eNtqimWhwQsTr__Npprv7O26GBC8Yf")
+    
+    #result2 = openPlaylist("PLg7eNtqimWhwQsTr__Npprv7O26GBC8Yf")
 
 
 if __name__ == "__main__":
