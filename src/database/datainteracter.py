bbe551fcbcaae96c3de97513de4df804a6563c79
heagge <henning.agge@gmail.com>
2026-02-13 07:54:42 +0100

    Refactor database initialization and insertions; add prepared statements for better security

--- src/database/datainteracter.py
+++ src/database/datainteracter.py
@@ -3,45 +3,54 @@ import sqlite3
 
 
 def initDatabase():
-    #define connection and cursor
+
     connection = sqlite3.connect("users_subscribers.db")
     cursor = connection.cursor()
-
-    # create tables
-    command = """CREATE TABLE IF NOT EXISTS users(owner TEXT PRIMARY KEY, topic TEXT, subscribers TEXT)"""
+    cursor.execute("DROP TABLE IF EXISTS users")
+    command = """CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    owner VARCHAR(255) NOT NULL,
+    topic VARCHAR(255) NOT NULL,
+    subscribers VARCHAR(255));"""
     cursor.execute(command)
 
-    #?macht owner nicht nur sin wen ich eine login habe oder den owner wechseln kann
-    #add data
-    cursor.execute("INSERT INTO users VALUES ('Henning Agge', 'LikedVideos', 'liked')")
-    cursor.execute("INSERT INTO users VALUES ('Henning Agge', 'Frontend', 'UCFbNIlppjAuEX4znoulh0Cw')")
-    cursor.execute("INSERT INTO users VALUES ('Henning Agge', 'Backend', 'UC4SVo0Ue36XCfOyb5Lh1viQ UC8butISFwT-Wl7EV0hUK0BQ UC9HOZ53gnHP3f_b-wixS74g')")
-    cursor.execute("INSERT INTO users VALUES ('Henning Agge', 'Bible/Religion', 'UCRKZqt7rf6_Lo5rFwuPTKmw UCTzZ2-byV7kigoeQ1ZQ39ig UCkY7QYZf3_ZnGQM6K_ju2aQ')")
-    cursor.execute("INSERT INTO users VALUES ('Henning Agge', 'ML/AI/LLM', '')")
-    cursor.execute("INSERT INTO users VALUES ('Henning Agge', 'Engeeniring/Architekture', 'UCQnJlfHdJ2OolZSa4h2H5Rg UCaSCt8s_4nfkRglWCvNSDrg')")
-    cursor.execute("INSERT INTO users VALUES ('Henning Agge', 'Motivation', 'UC5--wS0Ljbin1TjWQX6eafA UCsBjURrPoezykLs9EqgamOA')")
-    cursor.execute("INSERT INTO users VALUES ('Henning Agge', 'CyberSec', 'UC6biysICWOJ-C3P4Tyeggzg UC7e_BXvNfjKFCgqR4LkUe9A UC9c12UnfHIru-tZTbW-G3gQ UCJC0LxyEnw9yenVQxYanRzw')")
-    cursor.execute("INSERT INTO users VALUES ('Henning Agge', 'PersonalDevelopment', 'UCZe_ogqn3ZGC77M5gvk9dow ')")
-    cursor.execute("INSERT INTO users VALUES ('Henning Agge', 'AI SLOP', '')")
-    cursor.execute("INSERT INTO users VALUES ('Henning Agge', 'Hardware', 'UCGKEMK3s-ZPbjVOIuAV8clQ')")
-    cursor.execute("INSERT INTO users VALUES ('Henning Agge', 'News/Nachrichten', 'UC5NOEUbkLheQcaaRldYW5GA UCsBjURrPoezykLs9EqgamOA')")
-    cursor.execute("INSERT INTO users VALUES ('Henning Agge', 'Finace/Finanzen', 'UCFxPUPyzQQYaNfj0El4Ccqg UCeARcCUiZg79SQQ-2_XNlXQ UCkCGANrihzExmu9QiqZpPlQ UCkcnYVAVZQOB-nXHechtXDg')")
-    cursor.execute("INSERT INTO users VALUES ('Henning Agge', 'General', 'UC2Rxu8zyppEhjhZLlYL_iOQ UC8butISFwT-Wl7EV0hUK0BQ UCUyeluBRhGPCW4rPe_UvBZQ UCyFWoLmPTgZ3BkHIKMRSV1g')")
+
+    cursor.execute("INSERT INTO users VALUES (1,'Henning Agge', 'LikedVideos', 'liked')")
+    cursor.execute("INSERT INTO users VALUES (2,'Henning Agge', 'Frontend', 'UCFbNIlppjAuEX4znoulh0Cw')")
+    cursor.execute("INSERT INTO users VALUES (3,'Henning Agge', 'Backend', 'UC4SVo0Ue36XCfOyb5Lh1viQ UC8butISFwT-Wl7EV0hUK0BQ UC9HOZ53gnHP3f_b-wixS74g')")
+    cursor.execute("INSERT INTO users VALUES (4,'Henning Agge', 'test', 'UCRKZqt7rf6_Lo5rFwuPTKmw UCTzZ2-byV7kigoeQ1ZQ39ig UCkY7QYZf3_ZnGQM6K_ju2aQ')")
+    cursor.execute("INSERT INTO users VALUES (5,'Henning Agge', 'ML/AI/LLM', '')")
+    cursor.execute("INSERT INTO users VALUES (6,'Henning Agge', 'Engeeniring/Architekture', 'UCQnJlfHdJ2OolZSa4h2H5Rg UCaSCt8s_4nfkRglWCvNSDrg')")
+    cursor.execute("INSERT INTO users VALUES (7,'Henning Agge', 'Motivation', 'UC5--wS0Ljbin1TjWQX6eafA UCsBjURrPoezykLs9EqgamOA')")
+    cursor.execute("INSERT INTO users VALUES (8,'Henning Agge', 'CyberSec', 'UC6biysICWOJ-C3P4Tyeggzg UC7e_BXvNfjKFCgqR4LkUe9A UC9c12UnfHIru-tZTbW-G3gQ UCJC0LxyEnw9yenVQxYanRzw')")
+    cursor.execute("INSERT INTO users VALUES (9,'Henning Agge', 'PersonalDevelopment', 'UCZe_ogqn3ZGC77M5gvk9dow ')")
+    cursor.execute("INSERT INTO users VALUES (10,'Henning Agge', 'AI SLOP', '')")
+    cursor.execute("INSERT INTO users VALUES (11,'Henning Agge', 'Hardware', 'UCGKEMK3s-ZPbjVOIuAV8clQ')")
+    cursor.execute("INSERT INTO users VALUES (12,'Henning Agge', 'News/Nachrichten', 'UC5NOEUbkLheQcaaRldYW5GA UCsBjURrPoezykLs9EqgamOA')")
+    cursor.execute("INSERT INTO users VALUES (13,'Henning Agge', 'Finace/Finanzen', 'UCFxPUPyzQQYaNfj0El4Ccqg UCeARcCUiZg79SQQ-2_XNlXQ UCkCGANrihzExmu9QiqZpPlQ UCkcnYVAVZQOB-nXHechtXDg')")
+    cursor.execute("INSERT INTO users VALUES (14,'Henning Agge', 'General', 'UC2Rxu8zyppEhjhZLlYL_iOQ UC8butISFwT-Wl7EV0hUK0BQ UCUyeluBRhGPCW4rPe_UvBZQ UCyFWoLmPTgZ3BkHIKMRSV1g')")
+    connection.commit()
 
 
 def createDbRecode(playlist1: str, playlist2: str, channelId: str):
     
     connection = sqlite3.connect("users_subscribers.db")
     cursor = connection.cursor()
-
+    #prepared staments
+    
     if playlist1 != "----":
-        currentChannel1 = cursor.execute(f"SELECT subscribers FROM users where topic is {playlist1}")
-        cursor.execute(f"INSERT INTO users VALUES ('Henning Agge', '{playlist1}', '{currentChannel1 + " " + channelId}')")
+        currentChannel1 = cursor.execute(f"SELECT subscribers FROM users where topic is ?", (playlist1))
+        cursor.execute(f"INSERT INTO users (owner, topic, subscribers) VALUES ('Henning Agge', ?, ?)", (playlist1, currentChannel1 + " " + channelId))
+        connection.commit()
 
     if playlist2 != "----":
-        currentChannel2 = cursor.execute(f"SELECT subscribers FROM users where topic is {playlist2}")
-        cursor.execute(f"INSERT INTO users VALUES ('Henning Agge', '{playlist2}', '{currentChannel2 + " " + channelId}')")
+        currentChannel2 = cursor.execute(f"SELECT subscribers FROM users where topic is ?", (playlist2))
+        cursor.execute(f"INSERT INTO users (owner, topic, subscribers) VALUES ('Henning Agge', ?, ?)",(playlist2, currentChannel2 + " " + channelId))
+        connection.commit()
 
 
-def checkChannelInDb(chnnelId):
+def checkChannelInDb(chnnelId: str):
+    pass
+
+def getChannelsForTopic(topic: str):
     pass
